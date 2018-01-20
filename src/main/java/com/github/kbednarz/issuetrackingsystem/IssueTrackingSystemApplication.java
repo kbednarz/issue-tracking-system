80ce415ebeda74f50ee8b74fe3a6da2d7f5fddcc
kamex737 <kamex737@gmail.com>
2018-01-20 18:30:50 +0100

    Initial commit

---
+++ src/main/java/com/github/kbednarz/issuetrackingsystem/IssueTrackingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.github.kbednarz.issuetrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingSystemApplication.class, args);
+	}
+}
