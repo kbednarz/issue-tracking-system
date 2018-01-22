967caefd078d5801aadb92828d2af153ebd43e5c
kamex737 <kamex737@gmail.com>
2018-01-22 19:16:15 +0100

    Added basic config for Spring security

---
+++ src/main/java/com/github/kbednarz/issuetrackingsystem/config/UrlResolver.java
@@ -0,0 +1,15 @@
+package com.github.kbednarz.issuetrackingsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class UrlResolver extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
