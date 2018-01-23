1606c2fcb35879beb93ea4b0d3d243f9b17bf0e9
kamex737 <kamex737@gmail.com>
2018-01-23 21:28:17 +0100

    Added task service, controller, repo

--- src/main/java/com/github/kbednarz/issuetrackingsystem/repository/TaskRepository.java
+++ src/main/java/com/github/kbednarz/issuetrackingsystem/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package com.github.kbednarz.issuetrackingsystem.repository;
 
 import com.github.kbednarz.issuetrackingsystem.domain.Task;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
 }
