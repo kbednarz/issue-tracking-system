65283495c25ef3900163d03337551ebb8ed2becf
paczarny <czarny.pawel@student.pk.edu.pl>
2018-01-30 19:11:11 +0100

    project repository

---
+++ src/main/java/com/github/kbednarz/issuetrackingsystem/controller/ProjectController.java
@@ -0,0 +1,9 @@
+package com.github.kbednarz.issuetrackingsystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/project")
+public class ProjectController {
+         }
