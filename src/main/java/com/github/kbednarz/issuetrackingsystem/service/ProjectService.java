c975ea61eba5a57369833403c8d50e418e992cb8
paczarny <czarny.pawel@student.pk.edu.pl>
2018-01-30 19:15:57 +0100

    project service

---
+++ src/main/java/com/github/kbednarz/issuetrackingsystem/service/ProjectService.java
@@ -0,0 +1,30 @@
+package com.github.kbednarz.issuetrackingsystem.service;
+
+import com.github.kbednarz.issuetrackingsystem.domain.Project;
+import com.github.kbednarz.issuetrackingsystem.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService implements CrudServiceInterface<Project> {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Override
+    public Project save(Project instance) {
+        return projectRepository.save(instance);
+    }
+
+    @Override
+    public Project get(Long id) {
+        return projectRepository.findOne(id);
+    }
+
+    @Override
+    public List<Project> listAll() {
+        return (List<Project>) projectRepository.findAll();
+    }
+}
